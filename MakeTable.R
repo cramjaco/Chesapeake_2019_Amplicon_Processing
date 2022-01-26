dcd79b6a9f812c4312566ca0fbaecb1a99c98544
Jacob Cram <jcram@umces.edu>
2022-01-26 12:28:44 -0500

    Deleted duplicate files not in directories

--- MakeTable.R
+++
@@ -1,4 +0,0 @@
-library(dada2)
-load("Merged.RData")
-seqtab <- makeSequenceTable(merged)
-save(seqtab, file = "seqtab.RData")
--- SlayChimeras.R
+++
@@ -1,9 +0,0 @@
-library(dada2)
-load(file = "seqtab.RData")
-
-seqtab_nochim <- removeBimeraDenovo(seqtab, verbose = TRUE, method = "pooled", multithread = TRUE)
-
-save(seqtab, seqtab_nochim, file = "seqtab_both.RData")
-
-sum(seqtab_nochim)/sum(seqtab)
-# 0.914
